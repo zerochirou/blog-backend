9aacc302039621e31b8d840baefb5bed6feb75f1
zerochirou <-force>
2025-09-13 15:58:13 +0700

    cange(apiMiddleware): mengganti x-api-key ke api-token

--- app/middleware/apikey_middleware.ts
+++ app/middleware/apikey_middleware.ts
@@ -4,7 +4,7 @@ import type { NextFn } from '@adonisjs/core/types/http'
 
 export default class ApikeyMiddleware {
   async handle(ctx: HttpContext, next: NextFn) {
-    const apiKey = ctx.request.header('x-api-key')
+    const apiKey = ctx.request.header('api-token')
     if (!apiKey || apiKey !== env.get('API_KEY')) {
       return ctx.response.unauthorized({ status: 'error', message: 'Invalid API key' })
     }
