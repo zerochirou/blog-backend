308fc80b0c28f0b6aa8a4c010a4a6c24165d4a57
zerochirou <-force>
2025-09-13 15:54:24 +0700

    change(router): menambahkan versioning api

--- start/routes.ts
+++ start/routes.ts
@@ -6,37 +6,41 @@ import { middleware } from './kernel.js'
 
 router
   .group(function () {
-    router.get('all', [CategoriesController, 'findAllCategories']).use(middleware.apikey())
-    router.post('create', [CategoriesController, 'createCategory'])
     router
-      .get('find/id/:id', [CategoriesController, 'findCategoryById'])
-      .where('id', router.matchers.uuid())
-    router
-      .delete('remove/id/:id', [CategoriesController, 'removeCategory'])
-      .where('id', router.matchers.uuid())
-  })
-  .prefix('category')
+      .group(function () {
+        router.get('all', [CategoriesController, 'findAllCategories']).use(middleware.apikey())
+        router.post('create', [CategoriesController, 'createCategory'])
+        router
+          .get('find/id/:id', [CategoriesController, 'findCategoryById'])
+          .where('id', router.matchers.uuid())
+        router
+          .delete('remove/id/:id', [CategoriesController, 'removeCategory'])
+          .where('id', router.matchers.uuid())
+      })
+      .prefix('category')
 
-router
-  .group(function () {
-    router.get('all', [PostsController, 'findAllPosts'])
     router
-      .get('find/category/:id', [PostsController, 'findPostsByCategory'])
-      .where('id', router.matchers.uuid())
-    router
-      .get('find/slug/:slug', [PostsController, 'findPostsBySlug'])
-      .where('slug', router.matchers.slug())
-    router.post('create', [PostsController, 'createPost'])
-    router
-      .delete('remove/id/:id', [PostsController, 'removePost'])
-      .where('id', router.matchers.uuid())
-  })
-  .prefix('post')
+      .group(function () {
+        router.get('all', [PostsController, 'findAllPosts'])
+        router
+          .get('find/category/:id', [PostsController, 'findPostsByCategory'])
+          .where('id', router.matchers.uuid())
+        router
+          .get('find/slug/:slug', [PostsController, 'findPostsBySlug'])
+          .where('slug', router.matchers.slug())
+        router.post('create', [PostsController, 'createPost'])
+        router
+          .delete('remove/id/:id', [PostsController, 'removePost'])
+          .where('id', router.matchers.uuid())
+      })
+      .prefix('post')
 
-router
-  .group(function () {
-    router.post('create_admin', [AuthController, 'createAdmin'])
-    router.post('login', [AuthController, 'login'])
-    router.get('me', [AuthController, 'me'])
+    router
+      .group(function () {
+        router.post('create_admin', [AuthController, 'createAdmin'])
+        router.post('login', [AuthController, 'login'])
+        router.get('me', [AuthController, 'me'])
+      })
+      .prefix('auth')
   })
-  .prefix('auth')
+  .prefix('api/v1')
